cd6f6c4280c96607a490210d5cc6ca31c35f3b26
K0sta2001 <cotireich@gmail.com>
2023-11-12 19:58:14 +0400

    Initial setup

---
+++ src/MainRoot/App.js
@@ -0,0 +1,12 @@
+import '../Style.css';
+import MainRoot from "./MainRoot.js"
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainRoot/MainRoot.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+export default function MainRoot() {
+  
+  
+  
+  return (
+    <div className="App-Container">
+
+    </div>
+  )
+}
