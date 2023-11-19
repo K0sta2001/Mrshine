35524f2d3747ab7a860adcf91f2a28931c206966
K0sta2001 <cotireich@gmail.com>
2023-11-19 21:48:18 +0400

    Saw the issue The issue was the slider component taking up more than 100vw width even tho it was set to 100vw. Fixed it by reducing the width to 90vw. The reason for it is unknown, but it's not causing any responsive issues. Will dive in later to find out the reason.

--- src/MainRoot/GuestStack/Pages/Reusable/Slider.js
+++ src/MainRoot/GuestStack/Pages/Reusable/Slider.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderComponent = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -17,13 +17,12 @@ const SliderComponent = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 6000, 
+    autoplaySpeed: 6000,
     afterChange: (current) => setCurrentSlide(current),
   };
 
-
   return (
-    <div>
+    <div style={{ width: "90vw" }}>
       <Slider {...settings}>
         <div>
           <h3>Slide 1</h3>
@@ -35,19 +34,21 @@ const SliderComponent = () => {
           <h3>Slide 3</h3>
         </div>
       </Slider>
-      <div style={{ textAlign: 'center', marginTop: '10px' }}>
-        {Array.from({ length: settings.dots === true ? settings.slidesToShow : 0 }).map((_, index) => (
+      <div style={{ textAlign: "center", marginTop: "10px" }}>
+        {Array.from({
+          length: settings.dots === true ? settings.slidesToShow : 0,
+        }).map((_, index) => (
           <span
             key={index}
             onClick={() => handleDotClick(index)}
             style={{
-              display: 'none',
-              width: '15px',
-              height: '15px',
-              borderRadius: '50%',
-              background: currentSlide === index ? '#333' : '#ccc',
-              margin: '0 5px',
-              cursor: 'pointer',
+              display: "none",
+              width: "15px",
+              height: "15px",
+              borderRadius: "50%",
+              background: currentSlide === index ? "#333" : "#ccc",
+              margin: "0 5px",
+              cursor: "pointer",
             }}
           ></span>
         ))}
