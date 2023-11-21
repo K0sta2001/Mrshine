dc3a5c119db5dca18d140a6b0c0eb0a5ac169e83
K0sta2001 <cotireich@gmail.com>
2023-11-21 17:25:24 +0400

    Build log warning Build log warned me to add rel parameter to the anchors for security reasons.

--- src/MainRoot/GuestStack/Pages/Reusable/Footer.js
+++ src/MainRoot/GuestStack/Pages/Reusable/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div>
-        <a href="https://www.facebook.com/MrShine.ge" target="_blank">
+        <a href="https://www.facebook.com/MrShine.ge" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faFacebook} className="fb-logo" />
         </a>
-        <a href="https://www.instagram.com/mrshine5525/" target="_blank">
+        <a href="https://www.instagram.com/mrshine5525/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faInstagram} className="insta-logo" />
         </a>
       </div>
